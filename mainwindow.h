918a1e8100dc1571af38f66d492d8b5b4debf76e
Cho-yeon <HoraceQuintusHoratiusFlaccus@gmail.com>
2020-05-08 17:29:15 +0900

    CREATE: mainwindow.h

---
+++ mainwindow.h
@@ -0,0 +1,45 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QSessionManager>
+#include <QPlainTextEdit>
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+    void LoadFile(const QString &);
+
+protected:
+    void closeEvent(QCloseEvent *) override;
+
+private slots:
+    void NewFile();
+    void Open();
+    bool Save();
+    bool SaveAs();
+    void About();
+    void DocumentWasModified();
+#ifndef QT_NO_SESSIONMANAGER
+    void CommitData(QSessionManager &);
+#endif
+
+private:
+    void CreateActions();
+    void CreateStatusBar();
+    void ReadSettings();
+    void WriteSettings();
+    bool MaybeSave();
+    bool SaveFile(const QString &);
+    void SetCurrentFile(const QString &);
+    QString StrippedName(const QString &);
+
+    QPlainTextEdit * TextEdit;
+    QString CurFile;
+};
+#endif // MAINWINDOW_H
